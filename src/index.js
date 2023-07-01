f9fa1d3d238e27b8530ca49d0d8934d933c9d9cb
Erfin-FlyingCow <noufalirfan02@gmail.com>
2023-07-01 20:55:04 +0700

    tambah Navbar

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Component/App'
-import Test from'./Component/Test'
+import Navbar from './Component/Navbar';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Test />
+    <Navbar />
     <App />
   </React.StrictMode>
 );
