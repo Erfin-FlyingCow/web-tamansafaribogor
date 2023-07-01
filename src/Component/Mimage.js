e286d053765bba48132451c04785d9ed6edcfb6e
Erfin-FlyingCow <noufalirfan02@gmail.com>
2023-07-01 23:21:05 +0700

    fix image

--- src/Component/Mimage.js
+++ src/Component/Mimage.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function Mimage () {
     return(
         <div className="conatiner bg-gray-950">
-            <img src="https://cdn.discordapp.com/attachments/1096614485231423568/1124729098774786088/Mimage.png" alt="mimage"></img>
+            <img src="https://cdn.discordapp.com/attachments/1096614485231423568/1124735787435577524/Mimage.png" alt="mimage"></img>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Navbar from './Component/Navbar';
 import Mimage from './Component/Mimage';
+import Footer from './Component/Footer';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,6 +11,7 @@ root.render(
   <React.StrictMode>
     <Navbar />
     <Mimage />
+    <Footer />
   </React.StrictMode>
 );
 
