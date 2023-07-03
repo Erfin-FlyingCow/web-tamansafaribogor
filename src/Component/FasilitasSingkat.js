fe8751dde64b5f504b38ea827976e8ca771db8b0
Erfin-FlyingCow <noufalirfan02@gmail.com>
2023-07-03 13:29:39 +0700

    ganti hover fasilitas singkat

--- src/Component/FasilitasSingkat.js
+++ src/Component/FasilitasSingkat.js
@@ -17,7 +17,7 @@ export default function FasilitasSingkat() {
     <div className="bg-gray-300">
       <div className="container mx-auto">
         <div className="flex flex-row space-x-6">
-          <div className="w-1/3 bg-gray-300 p-5 hover:bg-lime-500 relative">
+          <div className="w-1/3 p-5 hover:scale-110 relative">
             <img className="rounded-t-lg"
               src="https://cdn.discordapp.com/attachments/1096614485231423568/1124903651492757616/Screenshot_2023-07-01_131709.png"
               alt="Gambar Fasilitas"
@@ -29,7 +29,7 @@ export default function FasilitasSingkat() {
               </div>
             )}
           </div>
-          <div className="w-1/3 bg-gray-300 p-5 hover:bg-lime-500 relative">
+          <div className="w-1/3 p-5 hover:scale-110 relative">
             <img className="rounded-t-lg"
               src="https://cdn.discordapp.com/attachments/1096614485231423568/1124934281244966932/wahana_Permainan.png"
               alt="Gambar Fasilitas"
@@ -41,7 +41,7 @@ export default function FasilitasSingkat() {
               </div>
             )}
           </div>
-          <div className="w-1/3 bg-gray-300 p-5 hover:bg-lime-500 relative">
+          <div className="w-1/3 p-5 hover:scale-110 relative">
             <img className="rounded-t-lg"
               src="https://cdn.discordapp.com/attachments/1096614485231423568/1124943236348248126/Safari_Lodge.png"
               alt="Gambar Fasilitas"
